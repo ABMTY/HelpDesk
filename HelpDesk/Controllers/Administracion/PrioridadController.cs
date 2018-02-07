6a035fb3fb5599692689f699290c6ab006ec56cf
devluisangel <dev.luis@abmty.com.mx>
2018-02-07 17:03:20 -0600

    Implementacion de Diseño y Funcionalidad de los siguientes Catalogos Politicas, TipoSoporte, Prioridad, Estados, Areas, TipoUsuario y Zonas

---
+++ HelpDesk/Controllers/Administracion/PrioridadController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelpDesk.Controllers.Administracion
+{
+    public class PrioridadController : Controller
+    {
+        // GET: Prioridad
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HelpDesk/Controllers/Proceso/TiketsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelpDesk.Controllers.Proceso
+{
+    public class TiketsController : Controller
+    {
+        // GET: Tikets
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
