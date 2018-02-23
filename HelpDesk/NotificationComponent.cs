8d7c668e748b73e1d05f208a06f2f0b772f646ca
dperezs <dperezs1782@gmail.com>
2018-02-23 06:23:24 -0600

    Update Comentarios Supervisor y Agente

--- HelpDesk/NotificationComponent.cs
+++ HelpDesk/NotificationComponent.cs
@@ -58,7 +58,46 @@ namespace HelpDesk
             {
                 return dc.Contacts.Where(a => a.AddedOn > afterDate).OrderByDescending(a => a.AddedOn).ToList();
             }
+
         }
 
+        private void dependency_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            if (e.Type == SqlNotificationType.Change)
+            {
+                SqlDependency sqlDep = sender as SqlDependency;
+                sqlDep.OnChange -= dependency_OnChange;
+
+                var notificationHub = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
+                notificationHub.Clients.All.notify("added");
+                RegisterComments(DateTime.Now);
+            }
+        }
+
+        public void RegisterComments(DateTime currentTime)
+        {
+
+            string conStr = ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+            string sqlCommand = @"SELECT  [id_comentario],[comentario],[fechahora_comentario],[id_detalle_ticket],[imagen],[id_usuario] FROM [dbo].[comentarios] where [fechahora_comentario] > @Fecha";
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                SqlCommand cmd = new SqlCommand(sqlCommand, con);
+                cmd.Parameters.AddWithValue("@Fecha", currentTime);
+                if (con.State != System.Data.ConnectionState.Open)
+                {
+                    con.Open();
+                }
+                cmd.Notification = null;
+                SqlDependency sqlDep = new SqlDependency(cmd);
+                sqlDep.OnChange += dependency_OnChange;
+                /*----------------------------------------*/
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    //
+                }
+            }
+
+        }
     }
 }
