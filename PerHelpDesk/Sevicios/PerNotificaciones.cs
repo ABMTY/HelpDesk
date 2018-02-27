96012acaa53725d782b212e92e102fa8997bcdd7
dperezs <dperezs1782@gmail.com>
2018-02-27 09:43:41 -0600

    Anexo de estado en Obtener todos de Notificaciones

--- PerHelpDesk/Sevicios/PerNotificaciones.cs
+++ PerHelpDesk/Sevicios/PerNotificaciones.cs
@@ -25,7 +25,7 @@ namespace PerHelpDesk.Sevicios
 
                 comandoSelect.Connection = Conexion;
                 comandoSelect.CommandType = CommandType.Text;
-                comandoSelect.CommandText = "SELECT [id_notificacion],[id_ticket],[fecha],[id_usuario],[notificado],[asunto] FROM [dbo].[notificaciones] where notificado is null";                
+                comandoSelect.CommandText = "SELECT [id_notificacion],[id_ticket],[fecha],[id_usuario],[notificado],[asunto],[estado] FROM [dbo].[notificaciones] where notificado is null";                
                 
                 using (var dr = comandoSelect.ExecuteReader())
                 {
@@ -39,6 +39,7 @@ namespace PerHelpDesk.Sevicios
                         entidad.id_usuario = int.Parse(dr["id_usuario"].ToString());
                         if(dr["notificado"].ToString()!=string.Empty)
                             entidad.notificado = int.Parse(dr["notificado"].ToString());
+                        entidad.estado = dr["estado"].ToString();
                         lista.Add(entidad);
                     }
                 }
