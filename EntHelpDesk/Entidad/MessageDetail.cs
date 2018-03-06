a69b1b0fc29db683f0f2693b470681a5f2d88af2
dperezs <dperezs1782@gmail.com>
2018-03-06 08:12:26 -0600

    Eitidades Commun Chat

---
+++ EntHelpDesk/Entidad/MessageDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntHelpDesk.Entidad
+{
+    public class MessageDetail
+    {
+
+        public string UserName { get; set; }
+
+        public string Message { get; set; }
+    }
+}
